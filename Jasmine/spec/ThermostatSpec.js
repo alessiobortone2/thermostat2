3bf3c543b123f8e2c454926f36c6b5231eff09bc
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-08-30 15:29:33 +0100

    Powersave mode added

--- Jasmine/spec/ThermostatSpec.js
+++ Jasmine/spec/ThermostatSpec.js
@@ -6,21 +6,25 @@ describe('Thermostat', function() {
   });
 
 
-  it('should default to 20 degrees', function () {
+  it('should default to 20 degrees', function() {
     expect(thermostat.temperature).toBe(20);
   });
 
-  it('should increase with an up function', function () {
+  it('should increase with an up function', function() {
       thermostat.up(1);
       expect(thermostat.temperature).toBe(21);
   });
 
-  it('should decrease with a down function', function () {
+  it('should decrease with a down function', function() {
       thermostat.down(1);
       expect(thermostat.temperature).toBe(19);
   });
 
-  it('min temperature is 10 degrees', function () {
+  it('min temperature is 10 degrees', function() {
     expect(thermostat.MIN_TEMP).toBe(10)
+  });
+
+  it('in power saving mode max temp is 25', function() {
+    expect( function() {thermostat.up(8)}).toThrow("Powersave mode is on, can't go above 25");
   });
 });
--- Jasmine/src/Thermostat.js
+++ Jasmine/src/Thermostat.js
@@ -1,10 +1,17 @@
 function Thermostat(temperature) {
   this.temperature = 20;
-  this.MIN_TEMP = 10
+  this.MIN_TEMP = 10;
+  this.powersave = true;
 };
 
 Thermostat.prototype.up = function(unit) {
-  this.temperature += unit;
+  if(this.powersave && (this.temperature + unit) > 25) {
+   throw "Powersave mode is on, can't go above 25";
+ }
+  else
+   {
+     this.temperature += unit;
+   }
 };
 
 Thermostat.prototype.down = function(unit) {
