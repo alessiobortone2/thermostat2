52a8f06c97b6574ad1e621affd456cd81d4e5e0e
alessiobortone2 <alessiobortone2@gmail.com>
2017-08-30 15:05:38 +0100

    what a nice afternoon, second test passes

--- Jasmine/spec/ThermostatSpec.js
+++ Jasmine/spec/ThermostatSpec.js
@@ -10,11 +10,8 @@ describe('Thermostat', function() {
     expect(thermostat.temperature).toBe(20);
   });
 
-
   it('should increase with an up function', function () {
       thermostat.up(1);
-      expect(thermostat.temperature}).toBe(21);
-    });
-
-
+      expect(thermostat.temperature).toBe(21);
+  });
 });
--- Jasmine/src/Thermostat.js
+++ Jasmine/src/Thermostat.js
@@ -1,3 +1,7 @@
 function Thermostat(temperature) {
   this.temperature = 20;
 };
+
+Thermostat.prototype.up = function(unit) {
+  this.temperature += unit;
+};
